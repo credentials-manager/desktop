7a6cd41e4d614e109c91d766a83c0c0fbb7b3aba
David Brutoczki <bcz.david99@gmail.com>
2021-11-01 16:09:29 +0100

    add create-dialog component

---
+++ src/app/store/create-dialog/create-dialog.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateDialogRoutingModule } from './create-dialog-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CreateDialogRoutingModule
+  ]
+})
+export class CreateDialogModule { }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -7,9 +7,10 @@ import { StoreComponent } from "./store.component";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { CreateDialogComponent } from './create-dialog/create-dialog.component';
 
 @NgModule({
-  declarations: [StoreComponent],
+  declarations: [StoreComponent, CreateDialogComponent],
   imports: [
     CommonModule,
     StoreRoutingModule,
